2687cda64db6791e13a8d686531f61cd34ffed98
hkate <90506754+hkate@users.noreply.github.com>
2022-10-09 22:38:19 +0200

    Exercises

--- src/Exercises/ford_escort/CSVReader.java
+++ src/Exercises/ford_escort/CSVReader.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 public class CSVReader {
 
+
     public void printCars() throws FileNotFoundException {
         List<Car> carList = readCarsFromCSV();
 
@@ -41,6 +42,29 @@ public class CSVReader {
         car.setMileage(Integer.parseInt(fields[1]));
         car.setPrice(Integer.parseInt(fields[2]));
         return car;
+    }
+    public void Byyear(int year) throws FileNotFoundException{
+        List<Car> carList = readCarsFromCSV();
+        for(int i = 0; i < carList.size(); i++){
+            if (year == carList.get(i).getYear() )
+                System.out.println(carList.get(i));
+        };
+    }
+
+    public void Range(int first, int second) throws FileNotFoundException{
+        List<Car> carList = readCarsFromCSV();
+        for(int i = 0; i < carList.size(); i++){
+            if (first < carList.get(i).getMileage() && carList.get(i).getMileage() < second )
+                System.out.println(carList.get(i));
+        };
+    }
+
+    public void Price(int price) throws FileNotFoundException{
+        List<Car> carList = readCarsFromCSV();
+        for(int i = 0; i < carList.size(); i++){
+            if (price > carList.get(i).getPrice() )
+                System.out.println(carList.get(i));
+        };
     }
 }
 
--- src/Exercises/ford_escort/Main.java
+++ src/Exercises/ford_escort/Main.java
@@ -6,6 +6,18 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Exercises.ford_escort.CSVReader csvReader = new CSVReader();
-        csvReader.printCars();
+        //csvReader.printCars();
+
+        int year = 1997;
+        //csvReader.Byyear(year);
+
+        int first = 30;
+        int second = 50;
+        //csvReader.Range(first,second);
+
+        int price = 6000;
+        csvReader.Price(price);
+
     }
+
 }
